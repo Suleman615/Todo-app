dea180296131700402fd464efb494ed1d6491135
Suleman615 <sulemanbuzdar615@gmail.com>
2025-03-02 16:12:01 +0500

    database updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const MONGO_URI = 'mongodb://127.0.0.1:27017/todo';
-const MONGO_URI = "mongodb+srv://suleman:suleman@todo.raprf.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URI = "mongodb+srv://my_tasks:Ta$kMana9er@todo.raprf.mongodb.net/?retryWrites=true&w=majority"
 const connectDatabase = () => {
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
